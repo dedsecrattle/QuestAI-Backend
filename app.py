08eb6a1034c8f5ea5fbef34abae733c8a8b566ef
dedsecrattle <itsprabxxx@gmail.com>
2024-03-31 15:52:52 +0800

    Flask Backend

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, jsonify
+import vertexai
+from vertexai.generative_models import GenerativeModel, Part, FinishReason
+import vertexai.preview.generative_models as generative_models
+
+app = Flask(__name__)
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    data = request.get_json()
+    topic = data.get('topic', '')
+    subtopic = data.get('subtopic', '')
+    max_output_tokens = data.get('max_output_tokens', 3729)
+    temperature = data.get('temperature', 0.9)
+    top_p = data.get('top_p', 1)
+
+    harm_categories = {
+        generative_models.HarmCategory.HARM_CATEGORY_HATE_SPEECH: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        generative_models.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        generative_models.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    }
+
+    vertexai.init(project="cs3263-project", location="us-central1")
+    model = GenerativeModel("gemini-1.0-pro-001")
+    prompt = f"Generate content on the topic {topic} and subtopic {subtopic} in around 1000 words"
+    generation_config = {
+        "max_output_tokens": max_output_tokens,
+        "temperature": temperature,
+        "top_p": top_p,
+    }
+
+    responses = model.generate_content(
+        [prompt],
+        generation_config=generation_config,
+        safety_settings=harm_categories,
+        stream=True,
+    )
+
+    generated_content = ""
+    for response in responses:
+        generated_content += response.text
+
+    return jsonify({"generated_content": generated_content})
+
+if __name__ == '__main__':
+    app.run(debug=True)
